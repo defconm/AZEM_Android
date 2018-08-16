7ae3fe73b4e68b58e96f1f8b9d14b5a7b88bf250
defconm <defconm15@gmail.com>
2018-08-16 12:26:39 -0700

    Update LoginVM.java
    update to the vm branch

--- app/src/main/java/com/example/david/azem/LoginVM.java
+++ app/src/main/java/com/example/david/azem/LoginVM.java
@@ -6,12 +6,17 @@ import android.arch.lifecycle.MutableLiveData;
 import android.arch.lifecycle.ViewModel;
 
 public class LoginVM extends ViewModel {
-
+    //TODO: create apiservice that returns mock data for temperary use until the firebase database is set up with proper API endpoint
+    ApiService api = new ApiService();
+    
     public LoginVM()
     {
         LiveData<UserLoginInformation> userInformation = new MutableLiveData<>();
         LiveDate<LoginState> loginState = new MutableLiveData<>();
 
     }
-
+    public boolean loginAttempt(String user, String password)
+    {
+        return response = api.login(user,password);        
+    }
 }
